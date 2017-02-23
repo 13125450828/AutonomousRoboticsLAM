30e1fe6177268b55c19b34e51ff2f50e4f26bc43
Wesley Fisher <wesley_b_fisher@hotmail.com>
2017-02-23 15:27:00 -0500

    Created ICP algorithm and some vis helpers

--- icp/src/icpmain.cpp
+++ icp/src/icpmain.cpp
@@ -17,52 +17,78 @@ struct trans
 };
 
 // Parameters to be changed
-float test_scan_increment = 0.001;
+float test_scan_num = 10;
 float test_tx = 0.5;
 float test_ty = 0.5;
 float test_theta = 0.1;
 int outer_loop_count = 3;
+float grad_alpha = 0.001;
+float grad_beta = 0.001;
 
 bool new_params = false;
 void callback(icp::ICPConfig& config, uint32_t level) {
   
   // Load into actual variables
-  test_scan_increment = config.test_angle_increment;
+  test_scan_num = config.test_angle_num;
   test_tx = config.test_tx;
   test_ty = config.test_ty;
   test_theta = config.test_theta;
   outer_loop_count = config.outer_loop_count;
+  grad_alpha = config.grad_alpha;
+  grad_beta = config.grad_beta;
   new_params = true;
 }
 
 
-sensor_msgs::LaserScan CreateFakeLaserScan()
+void CreateFakeLaserScan(sensor_msgs::LaserScan* scan)
 {
-  sensor_msgs::LaserScan scan;
-  scan.header.frame_id = "map";
-  scan.angle_min = -0.5;
-  scan.angle_max = 0.5;
-  scan.angle_increment = test_scan_increment;
-  scan.range_min = 0;
-  scan.range_max = 10;
+  //sensor_msgs::LaserScan scan;
+  scan->header.frame_id = "map";
+  scan->angle_min = -0.5;
+  scan->angle_max = 0.5;
+  scan->angle_increment = (scan->angle_max - scan->angle_min)/test_scan_num;
+  scan->range_min = 0;
+  scan->range_max = 10;
 
-  int num_points = (scan.angle_max - scan.angle_min)/scan.angle_increment;
-  scan.ranges = std::vector<float>();
+  scan->ranges = std::vector<float>();
 
-  for (int i = 0; i < num_points; i++)
+  //ROS_INFO_STREAM("Adding points: " << test_scan_num << " of " << test_scan_num);
+  for (int i = 0; i < test_scan_num; i++)
   {
-    scan.ranges.push_back(5.0);
+    scan->ranges.push_back(3.0);
   }
 }
 
 
-sensor_msgs::PointCloud TransformScan(sensor_msgs::LaserScan scan_in)
+void TransformScan(sensor_msgs::LaserScan* scan, sensor_msgs::PointCloud* cloud)
 {
-  laser_geometry::LaserProjection projector_;
-  sensor_msgs::PointCloud cloud;
-  projector_.projectLaser(scan_in, cloud);
+  float x;
+  float y;
+  float z = 0.0;
 
-  return cloud;
+  cloud->header.frame_id = scan->header.frame_id;
+
+  //projector_.projectLaser(scan_in, cloud);
+  //ROS_INFO_STREAM("Starting scan iter");
+  int size = test_scan_num;
+  for(int i =0; i < size; i++)
+  {
+    float th = scan->angle_min + i* scan->angle_increment;
+    float c = cos(th);
+    float s = sin(th);
+    float r = scan->ranges.at(i);
+
+    geometry_msgs::Point32 p;
+    p.x = c*r;
+    p.y = s*s*r;
+    p.z = z;
+
+    //ROS_INFO_STREAM("Adding point " << i);
+    //ROS_INFO_STREAM("Adding point: " << i << " of " << size << " r=" << r << ", th=" << th);
+    cloud->points.push_back(p); 
+  }
+
+  //return cloud;
 }
 
 void TransformCloud(sensor_msgs::PointCloud* cloud, trans t)
@@ -100,16 +126,47 @@ trans GetPointGradient(float x1, float y1, float x2, float y2, trans curr)
   float a = 2*(c*x1 - s*y1 + curr.tx - x2);
   float b = 2*(c*y1 + s*x1 + curr.ty - y2);
 
+  //ROS_INFO_STREAM("a: " << a << ", b: " << b);
+  //ROS_INFO_STREAM("c: " << c << ", s: " << s);
+  //ROS_INFO_STREAM("x1: " << x1 << ", y1: " << y1);
+
   trans grad;
 
-  grad.tx = 1*a;
-  grad.ty = 1*b;
-  grad.theta = a*(-s*x1 -c*y2) + b*(c*x2 - s*y2);
+  grad.tx = (1*a)*grad_beta;
+  grad.ty = (1*b)*grad_beta;
+  grad.theta = (a*(-s*x1 -c*y1) + b*(c*x1 - s*y1))*grad_beta;
+
+  return grad;
 }
 
-trans GetFullGradient(std::vector<geometry_msgs::Point32> cloud1, std::vector<geometry_msgs::Point32> cloud2, std::vector<unsigned int> corr)
+trans GetFullGradient(std::vector<geometry_msgs::Point32> cloud1, std::vector<geometry_msgs::Point32> cloud2, std::vector<unsigned int> corr, trans t)
 {
 
+  trans grad;
+  grad.tx = 0;
+  grad.ty = 0;
+  grad.theta = 0;
+
+  for(int i = 0; i < cloud1.size(); i++)
+  {
+    int index = corr.at(i);
+    geometry_msgs::Point32 p1 = cloud1.at(i);
+    geometry_msgs::Point32 p2 = cloud2.at(i);
+
+    trans t_new = GetPointGradient(p1.x, p1.y, p2.x, p2.y, t);
+
+    //ROS_INFO_STREAM("Part Grad: " << t_new.tx << " " << t_new.ty << " " << t_new.theta);
+
+    grad.tx = grad.tx + t_new.tx*grad_alpha;
+    grad.ty = grad.ty + t_new.ty*grad_alpha;
+    grad.theta = grad.theta + t_new.theta*grad_alpha;
+  }
+
+  grad.tx = grad.tx / test_scan_num;
+  grad.ty = grad.ty / test_scan_num;
+  grad.theta = grad.theta / test_scan_num;
+
+  return grad;
 }
 
 std::vector<unsigned int> GetCorrespondances(std::vector<geometry_msgs::Point32> cloud1, std::vector<geometry_msgs::Point32> cloud2)
@@ -156,7 +213,7 @@ std::vector<unsigned int> GetCorrespondances(std::vector<geometry_msgs::Point32>
   return indices;
 }
 
-trans ICP(sensor_msgs::PointCloud* c1, sensor_msgs::PointCloud* c2)
+trans ICP(sensor_msgs::PointCloud* c1, sensor_msgs::PointCloud* c2, sensor_msgs::PointCloud* temp, ros::Publisher* pub)
 {
   // c2 is a transform from c1
   // find the transform c1 -> c2
@@ -169,12 +226,31 @@ trans ICP(sensor_msgs::PointCloud* c1, sensor_msgs::PointCloud* c2)
   {
     std::vector<unsigned int> corr = GetCorrespondances((*c1).points, (*c2).points);
 
-    trans grad = GetFullGradient((*c1).points, (*c2).points, corr);
+    trans grad = GetFullGradient((*c1).points, (*c2).points, corr, t_init);
+
+    ROS_INFO_STREAM("Full Grad: " << grad.tx << " " << grad.ty << " " << grad.theta);
+
+    trans vis_grad;
+    vis_grad.tx = -grad.tx;
+    vis_grad.ty = -grad.ty;
+    vis_grad.theta = -grad.theta;
+    TransformCloud(temp, vis_grad);
+    pub->publish(*temp);
+
+    t_init.tx = t_init.tx - grad.tx;
+    t_init.ty = t_init.ty - grad.ty; 
+    t_init.theta = t_init.theta - grad.theta;
+
+    sleep(0.5);
   }
+
+  sleep(0.5);
+  return t_init;
 }
 
 
 int main(int argc, char **argv) {
+  ROS_INFO_STREAM("Starting");
   ros::init(argc, argv, "icp_node");
   ros::NodeHandle n;
 
@@ -183,34 +259,70 @@ int main(int argc, char **argv) {
   dynamic_reconfigure::Server<icp::ICPConfig>::CallbackType f;
   f = boost::bind(&callback, _1, _2);
   server.setCallback(f);
+  ROS_INFO_STREAM("Dyn Rec Created");
 
   ros::Publisher test_pub1 = n.advertise<sensor_msgs::PointCloud>("/cloud_1", 5);
   ros::Publisher test_pub2 = n.advertise<sensor_msgs::PointCloud>("/cloud_2", 5);
   ros::Publisher test_pubres = n.advertise<sensor_msgs::PointCloud>("/cloud_res", 5);
+  ROS_INFO_STREAM("Publishers Created");
+
+  sleep(2);
 
   while(ros::ok())
   {
+    //ROS_INFO_STREAM("Creating Scans");
+    sensor_msgs::LaserScan scan1;
+    CreateFakeLaserScan(&scan1);
+    sensor_msgs::LaserScan scan2;
+    CreateFakeLaserScan(&scan2);
+
+    //ROS_INFO_STREAM("Creating Clouds");
+    laser_geometry::LaserProjection projector_;
+    //sensor_msgs::PointCloud cloud;
     
-    sensor_msgs::LaserScan scan1 = CreateFakeLaserScan();
-    sensor_msgs::LaserScan scan2 = CreateFakeLaserScan();
+    sensor_msgs::PointCloud cloud1;
+    TransformScan(&scan1, &cloud1);
+    sensor_msgs::PointCloud cloud2;
+    TransformScan(&scan2, &cloud2);
 
-    sensor_msgs::PointCloud cloud1 = TransformScan(scan1);
-    sensor_msgs::PointCloud cloud2 = TransformScan(scan2);
+    sensor_msgs::PointCloud cloud_disp;
+    TransformScan(&scan1, &cloud_disp);
+    //ROS_INFO_STREAM("Transfering data to clouds");
 
-    trans t;
-    t.tx = test_tx;
-    t.ty = test_ty;
-    t.theta = test_theta;
+    if(!scan1.ranges.empty() && !scan2.ranges.empty())
+    {
 
-    TransformCloud(&cloud2, t);
+      trans t;
+      t.tx = test_tx;
+      t.ty = test_ty;
+      t.theta = test_theta;
 
-    //trans new_t = ICP(&cloud1, &cloud2)
+      //ROS_INFO_STREAM("Transforming C2");
+      TransformCloud(&cloud2, t);
 
-    test_pub1.publish(cloud1);
-    test_pub2.publish(cloud2);
+      ROS_INFO_STREAM("Starting ICP");
 
-    //TransformCloud(&cloud1, new_t);
-    //test_pubres.publish(cloud1);
+      test_pub1.publish(cloud1);
+      test_pub2.publish(cloud2);
+
+      ros::Time t1 = ros::Time::now();
+      trans new_t = ICP(&cloud1, &cloud2, &cloud_disp, &test_pubres);
+      ros::Time t2 = ros::Time::now();
+
+      ROS_INFO_STREAM("ICP time (s): " << (t2 - t1).toSec());
+      ROS_INFO_STREAM("Final: " << new_t.tx << ", " << new_t.ty << ", " << new_t.theta);
+
+      
+
+      TransformCloud(&cloud1, new_t);
+      test_pubres.publish(cloud1);
+
+      sleep(5);
+    }
+    else
+    {
+      ROS_INFO_STREAM("Empty Scans");
+    }
 
 
     ros::spinOnce();
