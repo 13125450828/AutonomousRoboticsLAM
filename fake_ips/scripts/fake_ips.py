f1b5f8deab22bd35091ba62dd5749667cff4c6a9
Wesley Fisher <wesley_b_fisher@hotmail.com>
2017-03-04 13:16:13 -0500

    Misc edits

--- fake_ips/scripts/fake_ips.py
+++ fake_ips/scripts/fake_ips.py
@@ -20,15 +20,15 @@ class Converter():
     def __init__(self):
         rospy.init_node('fake_ips')
         self.sub = rospy.Subscriber("/gazebo/model_states", ModelStates, self.callback)
-        self.pub = rospy.Publisher("/indoor_pos", Pose, queue_size=10)
+        self.pub = rospy.Publisher("/indoor_pos", PoseStamped, queue_size=10)
 
-        self.out =Pose()
+        self.out =PoseStamped()
 
     def callback(self, msg):
         for n, p, t in zip(msg.name, msg.pose, msg.twist):
             #print(n)
             if n == 'mobile_base':
-                self.out = p
+                self.out.pose = p
                 #self.out.orientation = t
 
                 self.pub.publish(self.out)
